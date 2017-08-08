569f951a9b9a7a002307b6ffa58b427cad9a8737
DESKTOP-RKGUJDC\Dreifingerjoey <svenga@hotmail.de>
2017-08-08 21:02:44 +0200

    New styleupdate

--- Eclipse/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/LeagueOfStalk/LookUp.jsp
+++ Eclipse/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/LeagueOfStalk/LookUp.jsp
@@ -12,10 +12,16 @@
 
 	
 	<div class="summonerPreview">
-		<!--<img  src="PICTURES/testSummonerIcon.png" alt="" class="summomerIcon"> </img>-->
-		<img  src=${sIcon} alt="" class="summomerIcon" ></img>
-		<div class="summonerdata"><br>${name}<br>Level: ${lvl} <br>Solo: ${devS} <br>Flexi: ${devF} <br>WW-Rank: <br>Serverrank: <br>KDA <br></div>
+		<img  src="PICTURES/testSummonerIcon.png" alt="" class="summomerIcon"> </img>
+		<!--<img  src=${sIcon} alt="" class="summomerIcon" ></img>-->
+		<div class="summonerdata"><div class="summonerdata"><br>${name}<br>Level: ${lvl} <br>Solo: ${devS} <br>Flexi: ${devF} <br>Rank: ${rank}  <br>KDA: ${rank} <br></div></div>	
+	</div>
 	
+	<div class="achievements">
+		Achivements
+		<hr>
+		Icon1<br>Icon2<br>Icon3<br>Icon4<br>Icon5
+	</div>
 	
 </body>
 
--- Eclipse/LeagueOfStalk/src/com/srk/App/SummonerLookup.java
+++ Eclipse/LeagueOfStalk/src/com/srk/App/SummonerLookup.java
@@ -3,11 +3,13 @@ import net.rithms.riot.api.ApiConfig;
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
 import net.rithms.riot.api.endpoints.league.dto.LeagueList;
+import net.rithms.riot.api.endpoints.league.dto.LeaguePosition;
 import net.rithms.riot.constant.Platform;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,7 +28,7 @@ public class SummonerLookup extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 				
-				ApiConfig config = new ApiConfig().setKey("RGAPI-2211a952-3708-4adb-928c-5f992da2119e");
+				ApiConfig config = new ApiConfig().setKey("RGAPI-019066bd-c78d-473a-892c-15abe47bf257");
 				RiotApi api = new RiotApi(config);
 				String name = request.getParameter("summoner");
 				net.rithms.riot.constant.Platform platform = Platform.EUW; // hier noch dynamisch änderbar
@@ -49,14 +51,23 @@ public class SummonerLookup extends HttpServlet {
 
 	private void getSummonerData(RiotApi a,HttpServletRequest r, String name,net.rithms.riot.constant.Platform p ) throws RiotApiException {
 		net.rithms.riot.api.endpoints.summoner.dto.Summoner summoner = a.getSummonerByName(p, name);
-		List<LeagueList> league = a.getLeagueBySummonerId(p, summoner.getId());
-		String devS =league.get(0).getTier()+" "+league.get(0).getEntryBySummonerId(summoner.getId()).getRank()+" "+league.get(0).getEntryBySummonerId(summoner.getId()).getLeaguePoints();
-		String devF =league.get(1).getTier()+" "+league.get(1).getEntryBySummonerId(summoner.getId()).getRank()+" "+league.get(1).getEntryBySummonerId(summoner.getId()).getLeaguePoints();
+		long sId =  summoner.getId();
+		List<LeagueList> league = a.getLeagueBySummonerId(p, sId);
+		
+		String devS =league.get(0).getTier()+" "+league.get(0).getEntryBySummonerId(sId).getRank()+" "+league.get(0).getEntryBySummonerId(sId).getLeaguePoints();
+		String devF =league.get(1).getTier()+" "+league.get(1).getEntryBySummonerId(sId).getRank()+" "+league.get(1).getEntryBySummonerId(sId).getLeaguePoints();
+		
+		//needs rework
+		String rank="not implemented";
+		//
+		String kda="not implemented";
 		//set Data für Lookup
 		r.setAttribute("name", name.toUpperCase());
 		r.setAttribute("lvl", summoner.getSummonerLevel());
 		r.setAttribute("devS",devS );
 		r.setAttribute("devF",devF );
+		r.setAttribute("rank",rank );
+		r.setAttribute("kda", kda );
 		String iconSrc = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/"+summoner.getProfileIconId()+".png";
 		r.setAttribute("sIcon",iconSrc );	
 	}
